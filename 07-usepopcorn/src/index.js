716255d6699827ccfa824ad94ddbbd580c386ce8
Onkar Mahamuni <onkarmahamuni52@gmail.com>
2024-01-05 22:01:26 +0530

    Added component reusability, props as component api and propTypes using reusable star components

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
     <StarRating />
+    <Test />
   </React.StrictMode>
 );
